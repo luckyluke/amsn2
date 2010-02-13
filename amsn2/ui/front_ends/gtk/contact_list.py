a32549cd26d6f79371058100e823b3b2e27d5a1c
Luca Dariz <luca.dariz@gmail.com>
2010-02-13 13:18:49 +0100

    [gtk] fix adding a group with offline contacts, avoid gtk warning on the contact's popup menu

--- amsn2/ui/front_ends/gtk/contact_list.py
+++ amsn2/ui/front_ends/gtk/contact_list.py
@@ -336,7 +336,7 @@ class aMSNContactListWidget(base.aMSNContactListWidget, gtk.TreeView):
     def __on_button_click(self, source, event):
         # Detect a single right-click
         if event.type == gtk.gdk.BUTTON_PRESS and event.button == 3:
-            treepath = self.get_path_at_pos(event.x, event.y)
+            treepath = self.get_path_at_pos(int(event.x), int(event.y))
 
             if treepath:
                 path, tree_column, x, y = treepath
@@ -347,7 +347,7 @@ class aMSNContactListWidget(base.aMSNContactListWidget, gtk.TreeView):
                     self.grab_focus()
                     self.set_cursor(path, tree_column, 0)
                     menu = gtk.Menu()
-                    common.createMenuItemsFromView(menu,
+                    common.create_menu_items_from_view(menu,
                                 view.on_right_click_popup_menu.items)
                     menu.popup(None, None, None, event.button, event.time)
 
@@ -410,6 +410,10 @@ class aMSNContactListWidget(base.aMSNContactListWidget, gtk.TreeView):
             if gid not in self.groups:
                 giter = self.__search_by_id(gid)
                 self._model.remove(giter)
+                try:
+                    del self.contacts[gid]
+                except KeyError:
+                    pass
 
     def group_updated(self, groupview):
         if (groupview.uid == 0): groupview.uid = '0'
